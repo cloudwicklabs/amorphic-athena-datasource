53676a1801bbc6e756b7b6f52de25373154113ea
Stanislav Yotov <29090864+svyotov@users.noreply.github.com>
2023-07-11 14:56:08 +0100

    ok

--- src/amorphic-types.tsx
+++ src/amorphic-types.tsx
@@ -1,25 +1,10 @@
-// import React, { FC } from 'react';
-// import { FormEvent } from 'react-dom/node_modules/@types/react';
 import { SQLQuery } from './amorphic-aws-sdk';
-// import {
-//   DataSourceSettings,
-//   DataSourceJsonData,
-//   SelectableValue,
-//   DataSourcePluginOptionsEditorProps,
-// } from '@grafana/data';
-// import { InputActionMeta } from '@grafana/ui';
 
 import { InlineField, Input } from '@grafana/ui';
 import { AwsAuthDataSourceJsonData, AwsAuthDataSourceSecureJsonData } from './amorphic-aws-sdk';
 
 import React, { FC } from 'react';
-import {
-  DataSourceJsonData,
-  DataSourceSettings,
-  DataSourcePluginOptionsEditorProps,
-  SelectableValue,
-} from '@grafana/data';
-import { InputActionMeta } from '@grafana/ui';
+import { DataSourceSettings, DataSourcePluginOptionsEditorProps, SelectableValue } from '@grafana/data';
 import { FormEvent } from 'react-dom/node_modules/@types/react';
 
 export interface InlineInputProps extends DataSourcePluginOptionsEditorProps<{}, AwsAuthDataSourceSecureJsonData> {
@@ -55,118 +40,7 @@ export function InlineInput(props: InlineInputProps) {
   );
 }
 
-// import {
-//   // InlineInputProps,
-//   // InlineInput,
-//   // ConfigSelect,
-//   // ConnectionConfig,
-// } from './amorphic-aws-sdk';
-
-// import { DataSourceJsonData, DataSourceSettings, DataSourcePluginOptionsEditorProps, SelectableValue, DataQuery, DataSourceApi, QueryEditorProps, ScopedVars } from '@grafana/data';
-
-// export interface AwsAuthDataSourceSecureJsonData {
-//   accessKey?: string;
-//   secretKey?: string;
-//   sessionToken?: string;
-// }
-
-// declare enum AwsAuthType {
-//   AmorphicKeys = 'amorphic-keys',
-//   Keys = 'keys',
-//   Credentials = 'credentials',
-//   Default = 'default',
-//   EC2IAMRole = 'ec2_iam_role',
-//   /**
-//    * @deprecated use default
-//    */
-//   ARN = 'arn',
-// }
-
-// export interface AwsAuthDataSourceJsonData extends DataSourceJsonData {
-//   authType?: AwsAuthType;
-//   assumeRoleArn?: string;
-//   externalId?: string;
-//   profile?: string;
-//   defaultRegion?: string;
-//   endpoint?: string;
-// }
-
-// export interface ConfigSelectProps
-//   extends DataSourcePluginOptionsEditorProps<AwsAuthDataSourceJsonData, AwsAuthDataSourceSecureJsonData> {
-//   value: string;
-//   fetch: () => Promise<Array<string | SelectableValue<string>>>;
-//   onChange: (e: SelectableValue<string> | null) => void;
-//   dependencies?: string[];
-//   label?: string;
-//   'data-testid'?: string;
-//   hidden?: boolean;
-//   disabled?: boolean;
-//   allowCustomValue?: boolean;
-//   saveOptions: () => Promise<void>;
-//   autoFocus?: boolean;
-//   backspaceRemovesValue?: boolean;
-//   className?: string;
-//   invalid?: boolean;
-//   isClearable?: boolean;
-//   isMulti?: boolean;
-//   inputId?: string;
-//   showAllSelectedWhenOpen?: boolean;
-//   maxMenuHeight?: number;
-//   minMenuHeight?: number;
-//   maxVisibleValues?: number;
-//   menuPlacement?: 'auto' | 'bottom' | 'top';
-//   menuPosition?: 'fixed' | 'absolute';
-//   noOptionsMessage?: string;
-//   onBlur?: () => void;
-//   onCreateOption?: (value: string) => void;
-//   onInputChange?: (value: string, actionMeta: InputActionMeta) => void;
-//   placeholder?: string;
-//   width?: number;
-//   isOptionDisabled?: () => boolean;
-//   labelWidth?: number;
-// }
-
-// declare function ConfigSelect(props: ConfigSelectProps): JSX.Element;
-
-// export interface AwsAuthDataSourceSecureJsonData {
-//   accessKey?: string;
-//   secretKey?: string;
-//   sessionToken?: string;
-// }
-// declare type AwsAuthDataSourceSettings = DataSourceSettings<
-//   AwsAuthDataSourceJsonData,
-//   AwsAuthDataSourceSecureJsonData
-// >;
-
-// // export interface InlineInputProps extends DataSourcePluginOptionsEditorProps<{}, AwsAuthDataSourceSecureJsonData> {
-// //   value: string;
-// //   onChange: (e: FormEvent<HTMLInputElement>) => void;
-// //   label?: string;
-// //   tooltip?: string;
-// //   placeholder?: string;
-// //   'data-testid'?: string;
-// //   hidden?: boolean;
-// //   disabled?: boolean;
-// //   labelWidth?: number;
-// // }
-// // declare function InlineInput(props: InlineInputProps): JSX.Element;
 declare const DEFAULT_LABEL_WIDTH = 28;
-
-// export interface ConnectionConfigProps<
-//   J extends AwsAuthDataSourceJsonData = AwsAuthDataSourceJsonData,
-//   S = AwsAuthDataSourceSecureJsonData
-// > extends DataSourcePluginOptionsEditorProps<J, S> {
-//   standardRegions?: string[];
-//   loadRegions?: () => Promise<string[]>;
-//   defaultEndpoint?: string;
-//   skipHeader?: boolean;
-//   skipEndpoint?: boolean;
-//   children?: React.ReactNode;
-//   labelWidth?: number;
-// }
-// export {FC, DataSourcePluginOptionsEditorProps};
-// declare const ConnectionConfig: FC<ConnectionConfigProps>;
-// // declare const SIGV4ConnectionConfig: React.FC<DataSourcePluginOptionsEditorProps<any, any>>;
 
 export enum FormatOptions {
   TimeSeries,
